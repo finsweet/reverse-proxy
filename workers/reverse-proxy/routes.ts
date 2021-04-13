ed3b17b0e012eab05d3da8e9bfb569dbe78ce0a8
Sowemimo Taiwo <tsowems@gmail.com>
2021-04-13 07:17:50 +0100

    Reverse proxy

--- workers/reverse-proxy/routes.ts
+++ workers/reverse-proxy/routes.ts
@@ -17,13 +17,11 @@ export const index: Handler =  async function (req, res) {
 		const subdomainUrl:any = await domain(url)
 			try {
 					path.shift();
-					//const file = path[0].split('@')[0];
 					const file = path[0].match(/([^@.]*)/)[0];
 					const cdnpath = cdn.match(/[a-z]([^@]*)/)[0];
 
 					//check if it contains @
 					if (path[0].includes('@') && (file == cdnpath)) {
-						//const versions = path[0].match(/[\d\.]+/);
 						const versions = path[0].match(/\b([0-9][0-9.]*)\b/);
 						const version = versions[0].slice(0, -1);
 						const fileext = /(?:\.([^.]+))?$/.exec(path[0])[1];
@@ -58,8 +56,8 @@ export const index: Handler =  async function (req, res) {
 const  converttourl = async(url:any) => {
 	const trimurl = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "");
 	const nurl = trimurl;
-			const newurl = "https://www.".concat(nurl)
-			return(newurl)
+	const newurl = "https://www.".concat(nurl)
+	return(newurl)
 }
 
 const domain  = async(url:any) => {
