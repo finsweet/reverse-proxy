9bb34adb72007058d476e20e652e0c921245779e
Sowemimo Taiwo <tsowems@gmail.com>
2021-04-19 11:36:29 +0100

    first commit

--- workers/reverse-proxy/routes.ts
+++ workers/reverse-proxy/routes.ts
@@ -3,76 +3,66 @@ import type { Handler } from 'worktop';
 const data = require('./cfw.json');
 const cdn:any = data.globals.MAPPED_CDN.replace('ENV:', '');
 const subdomains:any = data.globals.SUBDOMAINS.replace('ENV:', '').split(/[/?#]/);
+const cdnpath = cdn.match(/[a-z]([^@]*)/)[0];
 //const exturl=JSON.stringify(cdn).match(/[*(http)]\D+[a-z]/);
 const exturl  = cdn.match(/((http|un)[^'}\s]*)/);
 
 
 export const index: Handler =  async function (req, res) {
 		const url = req.headers.get('host') || '';
-		const path = req.path.split('/');
-		const path2 = req.path.split('/');
-		path2.splice(0, 2);
-		const otherpaths = path2.join("/");
-		const requrl:any = await converttourl(req.url);
-		const subdomain = subdomains[0].includes(path[1]);
-		const subdomainUrl:any = await domain(url)
-			try {
-					path.shift();
-					const file = path[0].match(/([^@.]*)/)[0];
-					const cdnpath = cdn.match(/[a-z]([^@]*)/)[0];
-
+		const path = req.params.path1;
+		const path2 = req.params.path2;
+		const otherpath = req.params.wild;
+		const otherpaths= [path2, otherpath].join('/');
+		const requrl:any = converttourl(req.url);
+		const subdomain = subdomains[0].includes(path);
+		const subdomainUrl:any = domain(url)
+				
+					const file = path.match(/([^@.]*)/)[0];
 					//check if it contains @
-					if (path[0].includes('@') && (file == cdnpath)) {
-						const version = path[0].match(/\d+(\.\d+)+/);
-						const desturl =exturl[0].replace(new RegExp('\\X\\b'), version[0]);
-						const destinationurl = await urlwithoutwww(desturl)
-						//return Response.redirect(destinationurl);
+					if (path.includes('@') && (file == cdnpath)) {
+						const version = path.match(/\d+(\.\d+)+/)[0];
+						const desturl =exturl[0].replace(new RegExp('\\X\\b'), version);
+						const destinationurl = urlwithoutwww(desturl)
 						return fetch(destinationurl);
 					}
 
 					//if cdn includes filename without @version
 						else if  ((file == cdnpath) && !(path[0].includes('@'))) {
 						const desturl =exturl[0].replace(new RegExp('\\@X\\b'), '');
-						const destinationurl = await urlwithoutwww(desturl);
+						const destinationurl =  urlwithoutwww(desturl);
 						return fetch(destinationurl);
 						
 					}
-		} catch(e){
-				console.log( e );
-		}
+		
 
-		try {
 					//Convert path to Subdomain
 					if(subdomain){
-						const destinationURL = "https://"+path[0]+"."+subdomainUrl+"/"+otherpaths;	
-							
-							return await fetch(destinationURL)
+						const destinationURL = "https://"+path+"."+subdomainUrl+"/"+otherpaths;	
+							return fetch(destinationURL)
 						  }
 					//deliver origin URL
 					else {
-						return await fetch(url)
-						//return Response.redirect(url);
+						return fetch(url)
+					
 						}
-		} catch(e) {
-				console.log(e);
-		}
+		
 }
 
 
-
-const  converttourl = async(url:any) => {
+function  converttourl(url:any) {
 	const trimurl = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "");
 	const nurl = trimurl;
 	const newurl = "https://www.".concat(nurl)
 	return(newurl)
 }
-const  urlwithoutwww = async(url:any) => {
+function urlwithoutwww (url:any)  {
 	const trimurl = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "");
 	const nurl = trimurl;
 	const newurl = "https://".concat(nurl)
 	return(newurl)
 }
-const domain  = async(url:any) => {
+function domain (url:any) {
 	const urlParts =	url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "").split('/')[0];
 	const domain = urlParts;
 	return(domain)
