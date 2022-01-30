ce7ff6495077272ba25d1ff441ec407ef717e56c
Alex Iglesias <alexiglesias@finsweet.com>
2022-01-30 18:25:06 +0100

    Moved `api` folder to a new repo.

--- workers/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'worktop';
-import { listen } from 'worktop/cache';
-import { purgeCache } from './routes';
-
-const API = new Router();
-
-API.add('POST', '/purge-cache', purgeCache);
-
-listen(API.run);
